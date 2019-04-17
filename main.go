939cb51cf9df8f19de8fde4301c195c9be58e73d
Jack Spirou <jack@spirou.io>
2019-04-17 17:49:10 -0500

    adding earliest step

--- main.go
+++ main.go
@@ -2,35 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
 )
 
-// docker run <container> command args
-// go run main.go run command args
 func main() {
-	switch os.Args[1] {
-	case "run":
-		run()
-	default:
-		panic("what?")
-	}
-}
-
-func run() {
-	fmt.Printf("running %v\n", os.Args[2:])
-
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
-
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	must(cmd.Run())
-}
-
-func must(err error) {
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println("hello world")
 }
